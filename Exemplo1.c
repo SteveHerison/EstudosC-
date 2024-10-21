997a0366b369414a0dc13d1ff105e9cec822fd2f
SteveHerison <steveherisondev@gmail.com>
2024-10-21 20:19:24 -0300

    first commit

---
+++ Exemplo1.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>  // Esta linha é necessária para usar system()
+
+
+static int a = 0;  // variável global, alocação estática
+
+void incrementa(void) {
+    int b = 0; // variável local, alocação automática
+    static int c = 0; // variável local, alocação estática
+
+    printf("a: %d, b: %d, c: %d\n", a, b, c);
+    a++;
+    b++;
+    c++;
+}
+
+int main(void) {
+    int i;
+
+    for (i = 0; i < 5; i++)
+        incrementa();
+
+    system("pause");  // Pausa a execução para ver a saída
+
+    return 0;
+}
+
+
+
